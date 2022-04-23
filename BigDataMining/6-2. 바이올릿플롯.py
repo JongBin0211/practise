9f02a082c2eeb476ac3a7e888343af8fdf5df767
JongBin0211 <103484278+JongBin0211@users.noreply.github.com>
2022-04-23 16:30:54 +0900

    Update 6-2. 바이올릿플롯.py

--- BigDataMining/6-2. 바이올릿플롯.py
+++ BigDataMining/6-2. 바이올릿플롯.py
@@ -8,5 +8,5 @@ fish_multi = pd.read_csv("./data/3-3-2-fish_multi_2.csv")
 print(fish_multi)
 
 sns.violinplot(data = fish_multi, x = "species",y = "length", color = "blue")
-pit.savefig("violinplot")
+plt.savefig("violinplot")
 plt.show()
