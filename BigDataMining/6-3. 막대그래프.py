532f1cf6350b84298f77079edc012fbd8ff13900
JongBin0211 <103484278+JongBin0211@users.noreply.github.com>
2022-04-23 16:31:54 +0900

    Update 6-3. 막대그래프.py

--- BigDataMining/6-3. 막대그래프.py
+++ BigDataMining/6-3. 막대그래프.py
@@ -8,4 +8,5 @@ fish_multi = pd.read_csv("./data/3-3-2-fish_multi_2.csv")
 print(fish_multi)
 
 sns.barplot(data = fish_multi, x = "species",y = "length", color = "green")
+plt.savefig("barplot")
 plt.show()
