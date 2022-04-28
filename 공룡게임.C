202d9f64c4d067caa7e839e51d1664eff4622cbf
JongBin0211 <103484278+JongBin0211@users.noreply.github.com>
2022-04-28 10:34:28 +0900

    Update 공룡게임.C

--- 공룡게임.C
+++ 공룡게임.C
@@ -68,15 +68,15 @@ void DrawDino(int dinoY)
 void DrawTree(int treeX)
 {
 	GotoXY(treeX, TREE_BOTTOM_Y);
-	printf("$$$$$");
+	printf("▲▲▲▲");
 	GotoXY(treeX, TREE_BOTTOM_Y + 1);
-	printf(" $$ ");
+	printf(" ▲▲ ");
 	GotoXY(treeX, TREE_BOTTOM_Y + 2);
-	printf(" $$ ");
+	printf(" ▲▲ ");
 	GotoXY(treeX, TREE_BOTTOM_Y + 3);
-	printf(" $$ ");
+	printf(" ▲▲ ");
 	GotoXY(treeX, TREE_BOTTOM_Y + 4);
-	printf(" $$ ");
+	printf(" ▲▲ ");
 }
 
 //(v2.0) 충돌 했을때 게임오버 그려줌
